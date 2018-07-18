618652f6805346ac03e179bb7aafefd63eeaa0e6
Wes <wesyes@att.net>
2018-07-18 16:32:36 -0400

    Refactor code to save email and name of user to be sent to tcr-ui

--- googleauth/src/App.js
+++ googleauth/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
-import Login from './Login';
-
+import GoogleLogin from 'react-google-login';
 
 class App extends Component {
-  render() {
-    return (
-      <Login/>
-    );
+  state = {
+    email: '',
+    name: ''
   }
+  //If called by onSuccess, the response will be login info
+  //If called by onFailure, the response will be the error and popup will ask user to try again
+  responseGoogle= (response)=>{
+    console.log(response);
+    //Save email address to identify user
+    this.setState({email : response.profileObj.email});
+    this.setState({name : response.w3.ig})
+    console.log(this.state.email);
+    console.log(this.state.name);
+  }
+    //clientId is set to localhost:3000
+    //This will change if we use hairthereum or different address
+    render() {
+      return (
+        <GoogleLogin
+      clientId="973501436556-sn4e93j7h6mo0ab0ameka7k5dfabreg8.apps.googleusercontent.com"
+      buttonText="Login with Consensys email"
+      onSuccess={this.responseGoogle}
+      onFailure={this.responseGoogle}
+    />
+      );
+    }
 }
 
 export default App;
--- googleauth/src/index.js
+++ googleauth/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './Login'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-ReactDOM.render(<Login />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
